989af7110c96e7807b3a7ac1a047b9dac58cc702
CamelInATardis <camelinatardis@gmail.com>
2015-10-30 19:47:51 -0600

    Adding Init
    Added CSS, data, JS, and HTML.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function ToggleLeftNavVisible() {
+
+	if (document.getElementById("nav-left").style.marginLeft != "-20vw") {
+
+		document.getElementById("nav-left").style.marginLeft = "-20vw";
+
+	} else {
+
+		document.getElementById("nav-left").style.marginLeft = "0vw";
+
+	}
+
+}
